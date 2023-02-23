5950698224712c68f2577aed895f9d26ed7cecd5
baargava <baargav.gottam@gmail.com>
2023-02-23 18:47:11 +0530

    add

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -4,7 +4,7 @@ const Loading = () => {
   return (
     <div className="loading-container">
       <div className="lds-ripple">
-        <div>Loading....</div>
+        <div></div>
         <div></div>
       </div>
     </div>
